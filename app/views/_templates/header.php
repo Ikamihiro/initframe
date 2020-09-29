e594728c3ed9bb56d2fd3a95035c89daf9b8649b
João Santana <joogabrielsntn66@gmail.com>
2020-09-29 18:04:28 -0400

    Inserindo Skelenton CSS para resetar os estilos

--- app/views/_templates/header.php
+++ app/views/_templates/header.php
@@ -5,9 +5,15 @@
     <title>MINI</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URL_BASE ?>css/base.css">
 </head>
 <body>
-    <h1>Header</h1>
-    <br>
-    <a href="<?php echo URL_BASE ?>login/logout">Logout</a>
-    <br>
+    <header>
+        <div class="navbar">
+            <div class="navbar__brand">
+                <a href="<?php echo URL_BASE ?>">
+                    Heroes
+                </a>
+            </div>
+        </div>
+    </header>
--- public/index.php
+++ public/index.php
@@ -5,7 +5,8 @@ ob_start();
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
 
-if (file_exists(ROOT . 'vendor/autoload.php')) {
+if (file_exists(ROOT . 'vendor/autoload.php')) 
+{
     require ROOT . 'vendor/autoload.php';
 }
 
